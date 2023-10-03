0c64c4cf066ac7acb37b4d8882249d03ff2401ad
Bae Joon Hoo <joonhoo0524@naver.com>
2023-10-03 19:01:02 +0900

    please work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const { Command } = require('commander');
 const program = new Command();
 
@@ -6,7 +7,7 @@ const fs = require("fs")
 program
     .name("ezpack")
     .description("Easiest way to pack a minecraft modpacks")
-    .version("1.0.1")
+    .version("1.0.2")
 
 fs.readdirSync(__dirname+"/commands/").forEach((file: string) => {
     const command = require(__dirname+"/commands/"+file)
